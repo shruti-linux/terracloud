2d45016681543de6427cca2725fec1203f363c7d
shruti-linux <shrutikulshreshta@gmail.com>
2025-04-16 13:37:25 +0530

    terraform

---
+++ provider.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  features {
+  }
+subscription_id = "853976da-1943-482f-be67-2d729c61ef50"
+}
+resource "azurerm_resource_group" "example21" {
+  name     = "rg-shruti-cloud"
+  location = "westus"
+}
+
