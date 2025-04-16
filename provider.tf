bb4ff7830d25f08bca7e3b440ac8de54c0f2c0c0
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2025-04-16 14:38:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 subscription_id = "853976da-1943-482f-be67-2d729c61ef50"
 }
 resource "azurerm_resource_group" "example21" {
-  name     = "rg-shruti-cloud"
+  name     = "rg-shruti-cloud"12
   location = "westus"
 }
 
